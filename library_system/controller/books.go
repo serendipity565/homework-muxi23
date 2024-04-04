f3de63d65867db983102b0e9558a0cbc73bf24ac
serendipity565 <jiang040906@gmail.com>
2024-04-04 15:21:03 +0800

    优化并修改了HTTP 状态码码

--- library_system/controller/books.go
+++ library_system/controller/books.go
@@ -8,17 +8,24 @@ import (
 )
 
 func Find(w http.ResponseWriter, r *http.Request) {
-	//展示书籍
+	// 展示书籍
 	fmt.Fprintf(w, "%v", service.Listbooks())
-	//查找书籍
+
+	// 查找书籍
 	bookname := r.PostFormValue("bookname")
 	if strings.TrimSpace(bookname) == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "请重新输入书名！")
 		return
-	} else {
-		w.WriteHeader(http.StatusAccepted)
-		fmt.Fprintf(w, "%v", service.Findbooks(bookname))
+	}
+
+	book := service.Findbooks(bookname)
+	if strings.TrimSpace(book) == "" {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "找不到书籍：%s", bookname)
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%v", book)
 }
--- library_system/controller/user.go
+++ library_system/controller/user.go
@@ -22,23 +22,15 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	num := service.Finduser(username, password)
-	if num == 0 {
-		err := service.Register(username, password)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "注册失败！%v", err)
-			return
-		} else {
-			w.WriteHeader(http.StatusAccepted)
-			fmt.Fprintf(w, "注册成功！")
-			return
-		}
-	} else {
+	err := service.Register(username, password)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "用户已存在！")
+		fmt.Fprintf(w, "注册失败：%v", err)
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "注册成功！")
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -49,18 +41,17 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "输入用户名或密码为空！")
 		return
 	}
+
 	num := service.Finduser(username, password)
-	if num == 1 {
-		w.WriteHeader(http.StatusAccepted)
+	switch num {
+	case 1:
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "登入成功！")
-		return
-	} else if num == 0 {
+	case 0:
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "此用户不存在！")
-		return
-	} else {
+	default:
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "密码错误！")
-		return
 	}
 }
